360441e22ffc1f2144273211afe6bc09105f76d8
meaterick <89893748+meaterick@users.noreply.github.com>
2023-12-16 11:02:21 +0900

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -9,14 +9,16 @@ ax = fig.add_subplot(111, projection='3d')
 
 earth = planet()
 earth.position.set(0, 0, 0)
+earth.vector.set(0, 0, 0)
 
 moon = planet()
 moon.position.set(0, 167, 0)
+earth.vector.set(1, 1, 0)
 
 for i in np.arange(-100, 100, 1):
-    ax.plot(*moon.position.loc(), marker="o", markersize=4, color='g')
+    ax.plot(*moon.move(), marker="o", markersize=4, color='g')
     
-    ax.plot(*earth.position.loc(), marker="o", markersize=20, color='b')
+    ax.plot(*earth.move(), marker="o", markersize=20, color='b')
     
     ax.plot(*(-100, -100, -100), marker="o", markersize=1, color='r')
     ax.plot(*(100, 100, 100), marker="o", markersize=1, color='r')
