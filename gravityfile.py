1e0b4d6cf70e3f15d709e91495ed745b412c427d
meaterick <inchanshome@gmail.com>
2023-12-17 22:43:18 +0900

    ss

---
+++ gravityfile.py
@@ -0,0 +1,10 @@
+class gravity():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        
+    def set(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
